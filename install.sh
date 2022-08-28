69c19a830224fcb9a7a098543035e5a9344a2a16
August Huber <augustalexander27@gmail.com>
2022-08-27 21:51:23 -0400

    feat: install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+mkdir -p ~/.local/share/gedit/styles
+
+cp themes/catppuccin-frappe.xml ~/.local/share/gedit/styles
+cp themes/catppuccin-latte.xml ~/.local/share/gedit/styles
+cp themes/catppuccin-macchiato.xml ~/.local/share/gedit/styles
+cp themes/catppuccin-mocha.xml ~/.local/share/gedit/styles
+
+exit 0
